4bc385e0805b125370e5988cc1bdfed249c12483
c-auri <aurichchristoph@gmail.com>
2022-04-01 15:07:35 +0200

    Add order 'Helles Taiwan, Dunkles Taiwan'

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -12,7 +12,6 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
 
     if (n > slides.length) {
         slideIndex = 1
@@ -25,10 +24,6 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
 } 
