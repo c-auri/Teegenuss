ba5e04b7cc1547dad41841162b5d95a219eb0277
c-auri <aurichchristoph@gmail.com>
2022-05-05 16:59:26 +0200

    Improve glossary accordion styles

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,14 +1,33 @@
-var accordions = document.getElementsByClassName("accordion");
+for (const panel of document.getElementsByClassName("glossary__panel")) {
+  collapse(panel);
+}
 
-for (var i = 0; i < accordions.length; i++) {
-  accordions[i].addEventListener("click", function() {
-    this.classList.toggle("accordion--active");
-    var panel = this.nextElementSibling;
+for (const header of document.getElementsByClassName("glossary__header")) {
+  toggleStyles(header);
+  header.addEventListener("click", togglePanelVisibilty);
+}
 
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-} 
+function togglePanelVisibilty() {
+  toggleStyles(this);
+  var panel = this.nextElementSibling;
+
+  if (panel.getAttribute("data-visible") === "true") {
+    collapse(panel);
+  } else {
+    expand(panel);
+  } 
+}
+
+function toggleStyles(header) {
+  header.classList.toggle("glossary__header--expanded");
+}
+
+function collapse(panel) {
+  panel.style.maxHeight = '0';
+  panel.setAttribute("data-visible", "false");
+}
+
+function expand(panel) {
+  panel.setAttribute("data-visible", "true");
+  panel.style.maxHeight = panel.scrollHeight + 'px';
+}
