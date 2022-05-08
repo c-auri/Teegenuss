ad425e81bf300b1e14d1ded54638a74f37f44599
c-auri <aurichchristoph@gmail.com>
2022-05-08 15:01:19 +0200

    Move tea details toggle into help section

--- assets/js/toggle-details.js
+++ assets/js/toggle-details.js
@@ -1,19 +1,22 @@
-const toggle = document.querySelector('.tea__description__toggle');
+const toggle = document.querySelector('.toggle');
 
 toggle.addEventListener('click', function() {
-  const text = document.querySelector('.tea__description__text');
-  const details = document.querySelector('.tea__description__details');
-  const status = document.querySelector('.tea__description__status');
+  const brewing = document.querySelector('.tea__brewing');
+  const details = document.querySelector('.tea__details');
+  const status = document.querySelector('.toggle__status');
+  const header = document.querySelector('#details');
 
-  if (details.getAttribute('data-visible') === "true") {
-    text.setAttribute("data-visible", "true");
+  if (status.getAttribute('data-position') === "left") {
+    brewing.setAttribute("data-visible", "true");
     details.setAttribute("data-visible", "false");
-    status.setAttribute("data-active", "false");
-    status.setAttribute("title", "Zeige Details");
+    status.setAttribute("data-position", "right");
+    status.setAttribute("title", "Zeige Zubereitung");
+    header.innerHTML = "Zubereitung";
   } else {
-    text.setAttribute("data-visible", "false");
+    brewing.setAttribute("data-visible", "false");
     details.setAttribute("data-visible", "true");
-    status.setAttribute("data-active", "true");
-    status.setAttribute("title", "Zeige Beschreibung");
+    status.setAttribute("data-position", "left");
+    status.setAttribute("title", "Zeige Details");
+    header.innerHTML = "Details";
   }
 });
