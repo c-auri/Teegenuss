bed2b863fd9a362fc0136837418286e6a177c0cf
Christoph Aurich <dev.aurich@gmail.com>
2022-05-29 14:27:33 +0200

    Rearrange ui js files

--- assets/js/ui/tea.js
+++ assets/js/ui/tea.js
@@ -1,4 +1,7 @@
 import { show, hide } from "/assets/js/utils/visibility.js"
+import { mod } from "/assets/js/utils/math.js"
+
+// ------ Toggle Details ------ //
 
 const toggle = document.querySelector('.toggle');
 
@@ -22,3 +25,19 @@ toggle.addEventListener('click', function() {
         header.innerHTML = "Details";
     }
 });
+
+// -------- Slideshow --------- //
+
+const slides = document.getElementsByClassName("slide");
+const prevButtons = document.getElementsByClassName('slideshow-prev')
+const nextButtons = document.getElementsByClassName('slideshow-next')
+
+function showSlide(position) {
+  hide(...slides)
+  show(slides[position])
+}
+
+for (let i = 0; i < slides.length; i++) {
+  prevButtons[i].addEventListener("click", () => showSlide(mod(i-1, slides.length)))
+  nextButtons[i].addEventListener("click", () => showSlide(mod(i+1, slides.length)))
+}
