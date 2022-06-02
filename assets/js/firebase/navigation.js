335c86888d2b16d786874ec268a3275de04396d4
Christoph Aurich <dev.aurich@gmail.com>
2022-06-02 22:41:03 +0200

    Set user image on login

--- assets/js/firebase/navigation.js
+++ assets/js/firebase/navigation.js
@@ -1,10 +1,11 @@
 import { auth } from "/assets/js/firebase/app.js";
 import { showProfile, showAuth } from "/assets/js/ui/navigation.js"
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js"
+import { onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js"
 
 const monitorAuthState = async () => {
     onAuthStateChanged(auth, user => {
         if (user) {
+            updateProfile(user, { photoURL: "/assets/img/icons/user.png" })
             showProfile(user)
         }
         else {
