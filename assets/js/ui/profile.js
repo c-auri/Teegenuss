bc9080024fbe099a95652b1a45d2d9fdd732c05a
Christoph Aurich <dev.aurich@gmail.com>
2022-06-02 22:35:22 +0200

    Add user image to profile

--- assets/js/ui/profile.js
+++ assets/js/ui/profile.js
@@ -2,6 +2,7 @@ import { show } from "/assets/js/utils/visibility.js"
 
 const divWelcome = document.querySelector('#divWelcome')
 const spanName = document.querySelector('#spanName')
+const imgUser = document.querySelector('#imgUser')
 const pUnverified = document.querySelector('#pUnverified')
 const pVerified = document.querySelector('#pVerified')
 
@@ -10,6 +11,7 @@ export const btnLogout = document.querySelector('#btnLogout')
 export function showProfile(user) {
     show(divWelcome)
     spanName.textContent = user.displayName
+    imgUser.src = user.photoURL
     
     if (user.emailVerified) {
         show(pVerified)
