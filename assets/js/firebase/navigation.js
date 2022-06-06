07a4a73cf4d0ca4da8dfc49c5c951524f27686a2
Christoph Aurich <dev.aurich@gmail.com>
2022-06-06 11:39:59 +0200

    Revert "Set user image on login"
    This reverts commit 335c86888d2b16d786874ec268a3275de04396d4.

--- assets/js/firebase/navigation.js
+++ assets/js/firebase/navigation.js
@@ -1,11 +1,10 @@
 import { auth } from "/assets/js/firebase/app.js";
 import { showProfile, showAuth } from "/assets/js/ui/navigation.js"
-import { onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js"
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js"
 
 const monitorAuthState = async () => {
     onAuthStateChanged(auth, user => {
         if (user) {
-            updateProfile(user, { photoURL: "/assets/img/icons/user.png" })
             showProfile(user)
         }
         else {
