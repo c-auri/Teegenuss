3e36cf63ff82787de505757e1fc050f7e2a6744f
Christoph Aurich <dev.aurich@gmail.com>
2022-06-07 18:00:18 +0200

    Test and update mod

--- assets/js/utils/math.js
+++ assets/js/utils/math.js
@@ -1,3 +1,9 @@
 export function mod(n, m) {
-    return ((n % m) + m) % m
+    let r = n % m
+
+    if (r < 0) {
+        r = m > 0 ? r + m : r - m
+    }
+    
+    return r === -0 ? 0 : r
 }
---
+++ assets/js/utils/math.test.js
@@ -0,0 +1,35 @@
+import { mod } from './math.js'
+
+// n, m, result
+const cases = [
+  // n < -m < 0
+  [-6, 3, 0], [-5, 3, 1], [-4, 3, 2], 
+
+  // -m <= n < 0
+  [-3, 3, 0], [-2, 3, 1], [-1, 3, 2], 
+
+  // n == 0
+  [0, 3, 0], [0, -3, 0], 
+
+  // 0 < n < m
+  [0, 3, 0], [1, 3, 1], [2, 3, 2], [3, 3, 0],
+
+  // 0 < m < n
+  [4, 3, 1], [5, 3, 2], [6, 3, 0],
+
+  // m <= n < 0
+  [-1, -3, 2], [-2, -3, 1], [-3, -3, 0],
+
+  // m < 0 < n
+  [1, -3, 1], [2, -3, 2], [3, -3, 0]
+]
+
+describe("mod returns the euclidian remainder:", () => {
+  test.each(cases)(
+    "%p % %p = %p",
+    (n, m, expectedResult) => {
+      const actualResult = mod(n, m);
+      expect(actualResult).toBe(expectedResult);
+    }
+  );
+});
