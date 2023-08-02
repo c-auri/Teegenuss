4c4e836ff96c8026828409edc65c43eb5607ae24
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-02 06:59:45 +0200

    Move tea heading on small screens

--- src/components/tea/Slideshow.jsx
+++ src/components/tea/Slideshow.jsx
@@ -7,7 +7,7 @@ export default function Slideshow({ images }) {
     const showNext = () => setIndex(modulo(index + 1, images.length))
 
     return (
-        <div className="grid grid-rows-[auto,auto] grid-cols-2 text-6xl text-gray-200 sm:grid-cols-[auto,auto,auto] sm:items-center justify-center lg:justify-start">
+        <div className="grid grid-rows-[auto,auto] grid-cols-2 gap-8 text-6xl text-gray-200 sm:grid-cols-[auto,auto,auto] sm:items-center sm:gap-0 justify-center lg:justify-start">
             <span className="cursor-pointer select-none text-right hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showPrevious}>❮</span>
             <img width="600" height="600" className="w-xl col-span-2 max-w-screen aspect-square col-start-1 col-end-3 justify-self-center sm:row-start-1 sm:col-start-2 sm:col-end-3" src={images[index].src} />
             <span className="cursor-pointer select-none text-left hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showNext}>❯</span>
