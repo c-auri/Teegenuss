84dc8d044f747db7342463fa7e6b8b5a2e9e53cb
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-02 06:25:03 +0200

    Rewrite tea hero styles

--- src/components/tea/Slideshow.jsx
+++ src/components/tea/Slideshow.jsx
@@ -7,10 +7,10 @@ export default function Slideshow({ images }) {
     const showNext = () => setIndex(modulo(index + 1, images.length))
 
     return (
-        <div className="grid grid-rows-[auto,auto] grid-cols-2 text-6xl text-gray-200 sm:grid-cols-[auto,auto,auto] sm:items-center">
-            <span className="cursor-pointer select-none p-4 text-right hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showPrevious}>❮</span>
+        <div className="grid grid-rows-[auto,auto] grid-cols-2 text-6xl text-gray-200 sm:grid-cols-[auto,auto,auto] sm:items-center justify-center lg:justify-start">
+            <span className="cursor-pointer select-none text-right hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showPrevious}>❮</span>
             <img width="600" height="600" className="w-xl col-span-2 max-w-screen aspect-square col-start-1 col-end-3 justify-self-center sm:row-start-1 sm:col-start-2 sm:col-end-3" src={images[index].src} />
-            <span className="cursor-pointer select-none p-4 text-left hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showNext}>❯</span>
+            <span className="cursor-pointer select-none text-left hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showNext}>❯</span>
         </div>
     )
 }
