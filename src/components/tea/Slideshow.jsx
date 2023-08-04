18338153bab9a3d483dbb8990197476178ca2844
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-04 03:18:40 +0200

    Extract Control component in Slideshow

--- src/components/tea/Slideshow.jsx
+++ src/components/tea/Slideshow.jsx
@@ -8,11 +8,9 @@ export default function Slideshow({ images }) {
 
     return (
         <div className="grid grid-rows-[auto,auto] grid-cols-2 gap-8 text-6xl text-gray-200 sm:grid-cols-[auto,auto,auto] sm:items-center sm:gap-0 justify-center lg:justify-start">
-            <span className="cursor-pointer select-none text-right hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showPrevious}>❮</span>
-            {
-                images.map((image, index) => Image(image.src, index, current))
-            }
-            <span className="cursor-pointer select-none text-left hover:text-gray-400 row-start-2 sm:row-start-1" onClick={showNext}>❯</span>
+            { Control("❮", showPrevious) }
+            { images.map((image, index) => Image(image.src, index, current)) }
+            { Control("❯", showNext) }
         </div>
     )
 }
@@ -25,4 +23,11 @@ function Image(src, index, current) {
         height="600"
         className={(index === current ? "block" : "hidden") + " w-xl col-span-2 max-w-screen aspect-square col-start-1 col-end-3 justify-self-center sm:row-start-1 sm:col-start-2 sm:col-end-3"}
     />
+}
+
+function Control(label, onClick) {
+    return (
+        <span onClick={onClick} className="cursor-pointer select-none text-right hover:text-gray-400 row-start-2 sm:row-start-1" >
+            {label}
+        </span>)
 }
