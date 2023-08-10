f88df2a8048a3a0fc623f413dd789e464639d0a9
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-10 19:21:52 +0200

    Merge Footer contents

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function({ addComponents }) {
+			addComponents({
+				'.button': {
+					borderRadius: '8px',
+					border: '1px solid #9ca3af',
+					padding: '.5rem 1rem',
+					fontWeight: 'bold',
+					color: '#9ca3af',
+					backgroundColor: 'white',
+					'&:hover': {
+						borderColor: '#737373',
+						backgroundColor: '#737373',
+					}
+				}
+			})
+		})
+	],
 }
