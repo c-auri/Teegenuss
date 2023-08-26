d1174aca064afab8b902dfec18cb42422e12ec2d
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-26 22:25:08 +0200

    Improve Breadcrumb spacing

--- src/components/page/Breadcrumbs.jsx
+++ src/components/page/Breadcrumbs.jsx
@@ -2,10 +2,10 @@ export default function Breadcrumbs({ navigation }) {
     if (!navigation) {
         return
     }
-    
+
     return navigation.segments.map((segment, segmentIndex) => (
         <div key={`segment-${segmentIndex}`} className="flex items-center">
-            <div className="relative group cursor-pointer pl-2 pr-4 py-1 rounded-md hover:bg-gray-200">
+            <div className="relative group cursor-pointer pl-2 pr-5 py-1 rounded-md hover:bg-gray-200">
                 <svg className="h-5 pr-1 inline fill-white group-hover:fill-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg>
                 <span className="group-hover:text-gray-500 ">{segment.entry.data.title}</span>
                 <ul className={`absolute left-0 top-[2rem] py-4 hidden group-hover:block hover:block`}>
@@ -14,7 +14,7 @@ export default function Breadcrumbs({ navigation }) {
                 }
                 </ul>
             </div>
-            <span className="pl-5">{segmentIndex < navigation.segments.length - 1 ? "/" : ""}</span>
+            <span className="pl-6">{segmentIndex < navigation.segments.length - 1 ? "/" : ""}</span>
         </div>))
 }
 
