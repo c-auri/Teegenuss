ba8e3fe18b7dc7d87ebafb7b51b4616eb819501a
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-26 21:44:56 +0200

    Add Dropdown to Breadcrumbs

--- src/components/page/Breadcrumbs.jsx
+++ src/components/page/Breadcrumbs.jsx
@@ -1,23 +1,57 @@
 export default function Breadcrumbs({ navigation }) {
-    if (!navigation.segments) 
+    if (!navigation) {
         return
-
-    return navigation.segments.map((segment, index) => (
-        <>
-            <a href={buildHref(navigation, index)} className="group cursor-pointer pl-2 pr-4 py-1 hover:text-gray-500 hover:bg-gray-200 hover:rounded-md">
-                <svg class="h-5 inline fill-white group-hover:fill-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg>
-                {segment.entry.data.title}
-            </a>
-            <span className="pl-1">{index < navigation.segments.length - 1 ? "/" : ""}</span>
-        </>))
+    }
+    
+    return navigation.segments.map((segment, segmentIndex) => (
+        <div key={`segment-${segmentIndex}`} className="flex items-center">
+            <div className="relative group cursor-pointer pl-2 pr-4 py-1 rounded-md hover:bg-gray-200">
+                <svg className="h-5 pr-1 inline fill-white group-hover:fill-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg>
+                <span className="group-hover:text-gray-500 ">{segment.entry.data.title}</span>
+                <ul className={`absolute left-0 top-[2rem] py-4 hidden group-hover:block hover:block`}>
+                {
+                    Dropdown(navigation, segmentIndex, segment.entry)
+                }
+                </ul>
+            </div>
+            <span className="pl-5">{segmentIndex < navigation.segments.length - 1 ? "/" : ""}</span>
+        </div>))
 }
 
-function buildHref(navigation, index) {
+function Dropdown(navigation, segmentIndex, currentEntry) {
+    const segment = navigation.segments[segmentIndex]
+
+    return segment.collection.map((entry, entryIndex) => {
+        let rounded = ""
+        
+        if (entryIndex === 0) {
+            rounded = "rounded-t-md pt-1"
+        } 
+        
+        if (entryIndex === segment.collection.length - 1) {
+            rounded = "rounded-b-md pb-1"
+        }
+
+        return <a 
+                    className={`${entry.id === currentEntry.id ? "text-gray-500 hover:text-gray-700" : ""} whitespace-nowrap`}
+                    href={Href(navigation, segmentIndex, entry)}
+                    key={`${entryIndex}`}
+                >
+            <li className={`${rounded} flex flex-col items-stretch py-1 pl-8 pr-6 bg-gray-100 hover:text-gray-700 hover:bg-gray-200`}>
+                {entry.data.title}
+            </li>
+        </a>
+    })
+}
+
+function Href(navigation, segmentIndex, entry) {
     let result = `/${navigation.root}`
 
-    for (let i = 0; i <= index; i++) {
+    for (let i = 0; i < segmentIndex; i++) {
         result += `/${navigation.segments[i].entry.data.route}`
     }
+
+    result += `/${entry.data.route}`
 
     return result
 }
