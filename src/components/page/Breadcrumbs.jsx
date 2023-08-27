975e8876fb83e69e561ec2737df4cbbfcc883564
c-auri <43008483+c-auri@users.noreply.github.com>
2023-08-27 11:39:11 +0200

    Add hover effect to segment entry

--- src/components/page/Breadcrumbs.jsx
+++ src/components/page/Breadcrumbs.jsx
@@ -4,12 +4,12 @@ export default function Breadcrumbs({ navigation }) {
     }
 
     return navigation.segments.map((segment, segmentIndex) => (
-        <div key={`segment-${segmentIndex}`} className="flex items-center select-none tracking-wider text-md text-gray-400 fill-gray-400 hover:fill-gray-500 hover:text-gray-500">
-            <div className="relative group cursor-pointer pl-2 pr-5 py-1 rounded-md hover:bg-gray-200">
-                <span className="">
+        <div key={`segment-${segmentIndex}`} className="group flex items-center select-none tracking-wider text-md text-gray-400 fill-gray-400 hover:fill-gray-500 hover:text-gray-500">
+            <div className="relative cursor-pointer rounded-md group-hover:bg-gray-100">
+                <div className="pl-2 pr-4 py-1 rounded-md hover:bg-gray-200">
                     <svg className="h-5 pr-1 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg>
                     {segment.entry.data.title}
-                </span>
+                </div>
                 <ul className={`absolute left-0 top-[2rem] py-4 hidden group-hover:block hover:block`}>
                 {
                     Dropdown(navigation, segmentIndex, segment.entry)
