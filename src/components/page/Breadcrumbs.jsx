547a45c8bc66fd788f8cc8e423f19bab3c368876
c-auri <43008483+c-auri@users.noreply.github.com>
2023-09-02 14:34:41 +0200

    Replace chevron up down in Breadcrumbs

--- src/components/page/Breadcrumbs.jsx
+++ src/components/page/Breadcrumbs.jsx
@@ -4,8 +4,10 @@ export default function Breadcrumbs({ navigation }) {
             navigation?.segments.map((segment, segmentIndex) => (
                 <div key={`segment-${segmentIndex}`} className="group flex items-center select-none tracking-wider text-md text-gray-400 fill-gray-400">
                     <div className="relative cursor-pointer rounded-md hover:fill-gray-500 group-hover:text-gray-500 group-hover:bg-gray-100">
-                        <div className="pl-2 pr-4 py-1 rounded-md hover:bg-gray-200">
-                            <svg className="h-5 pr-1 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" /></svg>
+                        <div className="pl-2 pr-4 py-1 rounded-md flex items-center gap-1 hover:bg-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" viewBox="0 -960 960 960">
+                                <path d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z"/>
+                            </svg>
                             <a href={Href(navigation, segmentIndex, segment.entry)}>{segment.entry.data.title}</a>
                         </div>
                         <ul className={`absolute left-0 top-full py-4 hidden group-hover:block hover:block`}>
