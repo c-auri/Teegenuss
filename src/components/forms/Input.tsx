88e721c83db0a277b715dea8f5fbef0f835d289a
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-19 23:05:54 +0100

    Add form components

---
+++ src/components/forms/Input.tsx
@@ -0,0 +1,41 @@
+interface Props {
+    type: string,
+    name: string,
+    label: string,
+    value: string,
+    handleChange?: (e: React.FormEvent<HTMLTextAreaElement | HTMLInputElement>) => void,
+    size?: number
+}
+
+export default function Input({
+    type,
+    name,
+    label,
+    value,
+    handleChange,
+    size
+}: Props) {
+    return <>
+        <label className={(size ? "" : "flex-1 ") + "flex flex-col"}>
+            <span className="pl-1 text-gray-600 text-md">{label}</span>
+            {
+                type === "textarea"
+                ? <textarea
+                    className={"px-2 py-1 border rounded-md text-lg text-gray-800 focus:outline-none focus:border-gray-600"}
+                    name={name}
+                    rows={size}
+                    value={value}
+                    onChange={handleChange}
+                />
+                : <input
+                    className={"px-2 py-1 border rounded-md text-lg text-gray-800 focus:outline-none focus:border-gray-600"}
+                    type={type}
+                    name={name}
+                    size={size}
+                    value={value}
+                    onChange={handleChange}
+                />
+            }
+        </label>
+    </>
+}
