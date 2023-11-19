562d3c90807e7de6ded4254f60006a64b0872352
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-19 23:55:11 +0100

    Add ContactForm

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -1,24 +1,24 @@
 import { useState } from 'react'
-import Input from '../forms/Input'
-import Button from '../forms/Button'
+import { ContactForm, type Contact } from './Contact'
 
 export default function Order() {
     const [ state, setState ] = useState({
-        current: "start",
         contact: {
             email: "",
+            discord: "",
+            source: "",
         },
     })
 
-    const setEmail = (email: string) => setState({ ...state, contact: {...state.contact, email: email}})
+    const setContact = (property: keyof Contact) => {
+        return (e: React.FormEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+            const newContact = {...state.contact}
+            newContact[property] = e.currentTarget.value
+            setState({...state, contact: newContact})
+        }
+    }
 
     return <>
-        <section>
-            <h2 className="font-bold text-lg text-gray-600">Kontakt</h2>
-            <form className="my-24 lg:my-32" onSubmit={(e) => { e.preventDefault(); console.log(state.contact)}}>
-                <Input type="email" label="Email" name="contact-email" value={state.contact.email} handleChange={(e) => setEmail(e.currentTarget.value)}/>
-                <Button type="submit">Weiter</Button>
-            </form>
-        </section>
+        <ContactForm values={state.contact} update={setContact}/>
     </>
 }
