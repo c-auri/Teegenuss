71e9cbe1ad5c65e2c799f8d42785526665f0d36b
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-20 20:47:16 +0100

    Add explanation to Input component

--- src/components/order/Contact.tsx
+++ src/components/order/Contact.tsx
@@ -36,7 +36,7 @@ export default function ContactForm({initialValues, handleSubmit}: Props) {
                 <h2 className="font-bold text-lg text-gray-600">Kontakt</h2>
                 <Input type="email" label="Email" name="contact-email" value={contact.email ?? ""} handleChange={update("email")} isRequired={true}/>
                 <Input type="text" label="Discord" name="contact-discord" value={contact.discord ?? ""} handleChange={update("discord")} isRequired={true}/>
-                <Input type="text" label="Woher kennen wir uns?" name="contact-source" value={contact.source ?? ""} handleChange={update("source")} isRequired={true}/>
+                <Input type="text" label="Woher kennen wir uns?" name="contact-source" value={contact.source ?? ""} handleChange={update("source")} explanation="kurzer Stichpunkt genügt" isRequired={true}/>
                 <Button type="submit">Weiter</Button>
             </form>
         </section>
