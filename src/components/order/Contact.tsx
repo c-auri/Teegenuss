b85cab69a7e99faa641da570e74354760ce0dab6
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-20 13:25:56 +0100

    Introduce state to Contact component

--- src/components/order/Contact.tsx
+++ src/components/order/Contact.tsx
@@ -1,28 +1,42 @@
 import Input from '../forms/Input'
 import Button from '../forms/Button'
 
+import { useState } from 'react'
+
 export type Contact = {
-    email?: string,
-    discord?: string,
-    source?: string
+    email: string,
+    discord: string,
+    source: string
 }
 
 type Props = {
-    values: Contact,
-    update: (property: keyof Contact) => (e: React.FormEvent<HTMLTextAreaElement | HTMLInputElement>) => void
+    initialValues: Contact,
+    handleSubmit: (contact: Contact) => void
 }
 
-export function ContactForm({values, update}: Props) {
+type UpdateEvent = React.FormEvent<HTMLTextAreaElement | HTMLInputElement>
+
+export function ContactForm({initialValues, handleSubmit}: Props) {
+    const [contact, setContact] = useState(initialValues)
+
+    const update = (property: keyof Contact) => {
+        return (e: UpdateEvent) => {
+            const newContact = {...contact}
+            newContact[property] = e.currentTarget.value
+            setContact(newContact)
+        }
+    }
+
     return <>
         <section>
-            <h2 className="font-bold text-lg text-gray-600">Kontakt</h2>
             <form
                 className="my-24 flex flex-col gap-6"
-                onSubmit={(e) => { e.preventDefault(); console.log(values)}}
+                onSubmit={(e) => { e.preventDefault(); handleSubmit(contact)}}
             >
-                <Input type="email" label="Email" name="contact-email" value={values.email ?? ""} handleChange={update("email")} isRequired={true}/>
-                <Input type="text" label="Discord" name="contact-discord" value={values.discord ?? ""} handleChange={update("discord")} isRequired={true}/>
-                <Input type="text" label="Woher kennen wir uns?" name="contact-source" value={values.source ?? ""} handleChange={update("source")} isRequired={true}/>
+                <h2 className="font-bold text-lg text-gray-600">Kontakt</h2>
+                <Input type="email" label="Email" name="contact-email" value={contact.email ?? ""} handleChange={update("email")} isRequired={true}/>
+                <Input type="text" label="Discord" name="contact-discord" value={contact.discord ?? ""} handleChange={update("discord")} isRequired={true}/>
+                <Input type="text" label="Woher kennen wir uns?" name="contact-source" value={contact.source ?? ""} handleChange={update("source")} isRequired={true}/>
                 <Button type="submit">Weiter</Button>
             </form>
         </section>
