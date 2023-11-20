abab638f68d3322104a31ef9eecde07ffa66c7ec
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-20 22:00:33 +0100

    Add name to ReadOnlyInput

--- src/components/order/Overview.tsx
+++ src/components/order/Overview.tsx
@@ -1,3 +1,4 @@
+import Button from "../forms/Button"
 import type { Contact } from "./ContactForm"
 
 type Props = {
@@ -8,21 +9,23 @@ export default function Overview({
     contact
 }: Props) {
     return <>
-        <form>
+        <form className="my-24 flex flex-col">
             <h2 className="font-bold text-xl text-gray-600">Übersicht</h2>
             <h3 className="font-bold text-lg text-gray-600">Kontakt</h3>
-            {ReadOnlyInput(contact.email)}
-            {ReadOnlyInput(contact.discord)}
-            {ReadOnlyInput(contact.source)}
+            {ReadOnlyInput("contact-email", contact.email)}
+            {ReadOnlyInput("contact-discord", contact.discord)}
+            {ReadOnlyInput("contact-source", contact.source)}
+            <Button type="submit">Bestellen</Button>
         </form>
     </>
 }
 
-function ReadOnlyInput(value: string) {
+function ReadOnlyInput(name: string, value: string) {
     return <>
         <label>
             <input
                 readOnly
+                name={name}
                 value={value}
                 className="text-lg text-gray-800 focus:outline-none"
             />
