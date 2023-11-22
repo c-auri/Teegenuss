b758cb12821ea9ffeb7ed5232ce441f5db022dba
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-22 18:30:35 +0100

    Add Address step

--- src/components/order/Overview.tsx
+++ src/components/order/Overview.tsx
@@ -1,22 +1,35 @@
 import Button from "../forms/Button"
+import type { Address } from "./Address"
 import type { Contact } from "./Contact"
 
 type Props = {
     contact: Contact,
+    address: Address,
 }
 
 export default function Overview({
-    contact
+    contact,
+    address,
 }: Props) {
     return <>
         <form className="my-24 flex flex-col" name="order" method="POST" data-netlify="true">
             <h2 className="font-bold text-xl text-gray-600">Übersicht</h2>
+        <input type="hidden" name="form-name" value="order" />
             <fieldset>
                 <h3 className="font-bold text-lg text-gray-600">Kontakt</h3>
-                <input type="hidden" name="form-name" value="order" />
                 {ReadOnlyInput("contact-email", contact.email)}
                 {ReadOnlyInput("contact-discord", contact.discord)}
                 {ReadOnlyInput("contact-source", contact.source)}
+            </fieldset>
+            <fieldset>
+                <h3 className="font-bold text-lg text-gray-600">Adresse</h3>
+                {ReadOnlyInput("address-name", address.name)}
+                {ReadOnlyInput("address-addition", address.addition)}
+                {ReadOnlyInput("address-street", address.street)}
+                {ReadOnlyInput("address-number", address.number)}
+                {ReadOnlyInput("address-postalCode", address.postalCode)}
+                {ReadOnlyInput("address-town", address.town)}
+                {ReadOnlyInput("address-country", address.country)}
             </fieldset>
             <Button type="submit">Bestellen</Button>
         </form>
