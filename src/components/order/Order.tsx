3d1053fdd628b2fc92813126fe44252958b1b7d2
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-24 18:58:40 +0100

    Add pack overview to Order

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -1,40 +1,75 @@
 import { useState } from 'react'
+import type { CollectionEntry } from 'astro:content'
 import { initializeContact, ContactStep } from './Contact'
 import { initializeAddress, AddressStep } from './Address'
 import Overview from './Overview'
 
-export default function Order() {
+type Props = {
+    pack: CollectionEntry<'packs'>,
+}
+
+export default function Order({ pack }: Props) {
     const [ current, setCurrent ] = useState("address")
     const [ contact, setContact ] = useState(initializeContact())
     const [ address, setAddress ] = useState(initializeAddress())
 
     return <>
-        <div className="max-w-lg">
+        <div className="flex flex-col gap-5">
 
-                <h1 className="my-14 lg:my-24 font-bold text-3xl text-center text-gray-700">Bestellung</h1>
+            <section className="max-w-lg py-5 px-3 rounded-md bg-gray-100 text-lg text-gray-600">
 
-            <AddressStep
-                isVisible={current === 'address'}
-                initialValues={address}
-                handleSubmit={setAddress}
-                goPrev={() => setCurrent("address")}
-                goNext={() => setCurrent("contact")}
-            />
+                <h1 className="py-2 font-bold text-2xl text-gray-700">
+                    Bestellübersicht
+                </h1>
 
-            <ContactStep
-                isVisible={current === 'contact'}
-                initialValues={contact}
-                handleSubmit={setContact}
-                goPrev={() => setCurrent("address")}
-                goNext={() => setCurrent("overview")}
-            />
+                <p className="flex justify-between">
+                    <span>{pack.data.title}</span>
+                    <span>{pack.data.price},00&thinsp;€</span>
+                </p>
+                <p className="flex justify-between">
+                    <span>Versand (Deutschland)</span>
+                    <span>3,00&thinsp;€</span>
+                </p>
 
-            <Overview
-                isVisible={current === 'overview'}
-                contact={contact}
-                address={address}
-                goPrev={() => setCurrent("contact")}
-            />
+                <div className="my-1 w-full h-px bg-gray-600"></div>
+
+                <p className="flex justify-between">
+                    <span>Gesamt</span>
+                    <span>{pack.data.price + 3},00&thinsp;€</span>
+                </p>
+
+                <p className="text-right text-sm text-gray-500">
+                    ggf. zzgl. Auslandsversandkosten
+                </p>
+
+            </section>
+
+            <section className="max-w-lg mb-10">
+
+                <AddressStep
+                    isVisible={current === 'address'}
+                    initialValues={address}
+                    handleSubmit={setAddress}
+                    goPrev={() => setCurrent("address")}
+                    goNext={() => setCurrent("contact")}
+                />
+
+                <ContactStep
+                    isVisible={current === 'contact'}
+                    initialValues={contact}
+                    handleSubmit={setContact}
+                    goPrev={() => setCurrent("address")}
+                    goNext={() => setCurrent("overview")}
+                />
+
+                <Overview
+                    isVisible={current === 'overview'}
+                    contact={contact}
+                    address={address}
+                    goPrev={() => setCurrent("contact")}
+                />
+
+            </section>
 
         </div>
     </>
