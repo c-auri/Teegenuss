02c6369c1c4ec8d6e218d6360b176cb1e7a1e32e
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-24 22:02:56 +0100

    Modify layout for order page

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -16,24 +16,24 @@ export default function Order({ pack }: Props) {
     return <>
         <div className="flex flex-col items-center gap-5 lg:mb-24 lg:flex-row lg:gap-0 lg:items-stretch lg:rounded-xl lg:bg-white lg:shadow-sm">
 
-            <section className="max-w-2xl w-screen p-3 rounded-md bg-gray-100 text-lg text-gray-600 lg:w-96 lg:rounded-none lg:rounded-l-xl lg:p-8">
+            <section className="max-w-2xl w-screen p-3 rounded-md bg-gray-100 text-lg text-gray-600 lg:min-w-fit lg:w-1/3 lg:rounded-none lg:rounded-l-xl lg:p-8 lg:pr-12">
 
                 <h1 className="pb-2 font-bold text-2xl text-gray-700">
                     Bestellung
                 </h1>
 
-                <p className="flex justify-between">
+                <p className="flex justify-between gap-8">
                     <span>{pack.data.title}</span>
                     <span>{pack.data.price},00&thinsp;€</span>
                 </p>
-                <p className="flex justify-between">
+                <p className="flex justify-between gap-8">
                     <span>Versand (Deutschland)</span>
                     <span>3,00&thinsp;€</span>
                 </p>
 
                 <div className="my-1 w-full h-px bg-gray-600"></div>
 
-                <p className="flex justify-between">
+                <p className="flex justify-between gap-8">
                     <span>Gesamt</span>
                     <span>{pack.data.price + 3},00&thinsp;€</span>
                 </p>
@@ -44,7 +44,7 @@ export default function Order({ pack }: Props) {
 
             </section>
 
-            <section className="max-w-2xl w-screen mb-10 border-gray-100 lg:rounded-r-xl lg:p-8">
+            <section className="max-w-2xl w-screen mb-10 border-gray-100 lg:rounded-r-xl lg:py-8 lg:px-12">
 
                 <AddressStep
                     isVisible={current === 'address'}
