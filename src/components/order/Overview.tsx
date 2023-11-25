1fad0b1c89db0311a6656c1bbc29108a16783e5e
c-auri <43008483+c-auri@users.noreply.github.com>
2023-11-25 12:32:04 +0100

    Update overview styles

--- src/components/order/Overview.tsx
+++ src/components/order/Overview.tsx
@@ -25,23 +25,30 @@ export default function Overview({
             <input type="hidden" name="form-name" value="order" />
 
             <div className="flex-1">
-                <div className="mb-6 flex justify-start">
+                <div className="mb-6 grid grid-cols-2 gap-y-8">
 
-                    <fieldset>
+                    <fieldset className="flex flex-col">
                         <h2 className="font-bold text-lg text-gray-600">Adresse</h2>
                         {ReadOnlyInput("address-name", address.name)}
-                        {ReadOnlyInput("address-annotation", address.annotation)}
+                        {address.annotation && ReadOnlyInput("address-annotation", address.annotation)}
                         {ReadOnlyInput("address-street", `${address.street} ${address.number}`)}
                         {ReadOnlyInput("address-town", `${address.postalCode} ${address.town}`)}
                         {ReadOnlyInput("address-country", address.country)}
                     </fieldset>
 
-                    <fieldset>
+                    <fieldset className="flex flex-col">
                         <h2 className="font-bold text-lg text-gray-600">Kontakt</h2>
                         {ReadOnlyInput("contact-discord", contact.discord)}
                         {ReadOnlyInput("contact-source", contact.source)}
-                        {ReadOnlyInput("contact-annotation", contact.annotation)}
                     </fieldset>
+
+                    {
+                        contact.annotation.trim() &&
+                        <fieldset className="col-span-2">
+                            <h2 className="font-bold text-lg text-gray-600">Anmerkung</h2>
+                            {ReadOnlyInput("contact-annotation", contact.annotation, "textarea")}
+                        </fieldset>
+                    }
 
                 </div>
             </div>
@@ -52,15 +59,29 @@ export default function Overview({
     </>
 }
 
-function ReadOnlyInput(name: string, value: string) {
-    return <>
-        <label>
-            <input
-                readOnly
-                name={name}
-                value={value}
-                className="text-lg text-gray-800 bg-transparent focus:outline-none"
-            />
-        </label>
-    </>
+function ReadOnlyInput(name: string, value: string, type: "text" | "textarea" = "text") {
+    if (type === "text") {
+        return <>
+            <label>
+                <input
+                    readOnly
+                    name={name}
+                    value={value}
+                    className="text-lg text-gray-800 bg-transparent focus:outline-none"
+                />
+            </label>
+        </>
+    } else {
+        return <>
+            <label>
+                <textarea
+                    readOnly
+                    name={name}
+                    value={value}
+                    className="w-full text-lg text-gray-800 bg-transparent focus:outline-none"
+                    rows={6}
+                />
+            </label>
+        </>
+    }
 }
