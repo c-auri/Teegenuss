fc2f994e8ac7b6f96143b2068054cc2cd5070117
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-28 23:07:32 +0100

    Change gray colors to slate

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -20,8 +20,8 @@ export default function Button({
                 href={href}
                 className=
                 {
-                    "cursor-pointer py-1 px-8 border-2 rounded-md text-lg hover:bg-gray-200 hover:text-gray-600 hover:border-gray-200 "
-                    + (style === "primary" ? " border-gray-100 bg-white text-gray-600" : " bg-white border-gray-100 text-gray-600")
+                    "cursor-pointer py-1 px-8 border-2 rounded-md text-lg hover:bg-slate-200 hover:text-slate-600 hover:border-slate-200 "
+                    + (style === "primary" ? " border-slate-100 bg-white text-slate-600" : " bg-white border-slate-100 text-slate-600")
                 }
             >
                 {children}
@@ -34,8 +34,8 @@ export default function Button({
                 onClick={handleClick}
                 className=
                 {
-                    "cursor-pointer py-1 px-8 border-2 rounded-md text-lg hover:bg-gray-200 hover:text-gray-600 border-gray-200 hover:border-gray-200 "
-                    + (style === "primary" ? "text-gray-600 bg-white" : "text-gray-500 bg-neutral-50")
+                    "cursor-pointer py-1 px-8 border-2 rounded-md text-lg hover:bg-slate-200 hover:text-slate-600 border-slate-200 hover:border-slate-200 "
+                    + (style === "primary" ? "text-slate-600 bg-white" : "text-slate-500 bg-neutral-50")
                 }
             >
                 {children}
