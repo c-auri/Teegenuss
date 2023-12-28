0ff6142bdfe98a862da32b687a04921990f3304f
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-28 13:15:52 +0100

    Change Terms list to list-inside

--- src/components/order/Terms.tsx
+++ src/components/order/Terms.tsx
@@ -18,7 +18,7 @@ export function TermsStep({isVisible, handleBack, handleNext}: Props) {
             <h2 className="pb-3 font-bold text-2xl text-gray-600">Teilnahmebedingungen</h2>
 
             <div className="mb-8 flex-1">
-                <ol className="list-decimal text-gray-800 flex flex-col justify-start gap-3">
+                <ol className="list-decimal list-inside text-gray-800 flex flex-col justify-start gap-3">
                     <li>Ich bin kein gewerblich eingetragener Händler und habe <span className="font-bold">keine Gewinnabsicht</span>.  Es handelt sich hier um einen freundschaftlichen Austausch, deshalb ist es mir auch wichtig jeden Teilnehmer zuordnen zu können.</li>
                     <li>Die Bezahlung ist nur per <span className="font-bold">Vorkasse</span> möglich. Der Bezahlvorgang ist nicht automatisiert. Nachdem ihr die Bestellung aufgegeben habt, melde ich mich zeitnah bei euch um die Bezahlung und weitere Details direkt auszumachen. Ich bevorzuge Bezahlung per <span className="font-bold">PayPal</span>, eine Banküberweisung ist aber auch möglich.</li>
                     <li>Das Paket wird aus Kostengründen <span className="font-bold">unversichtert und ohne Sendungsverfolgung mit der Briefpost</span> verschickt. Bisher ist dabei noch nie etwas schief gegangen, ihr tragt dabei als Empfänger aber das Risiko. Wer sicher gehen möchte kann sich gegen Aufpreis für eine andere Versandoption entscheiden.</li>
