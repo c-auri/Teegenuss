5ef1e7c06e717ad35e27869a3dd6ff21e274eaf8
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-29 22:30:37 +0100

    Fix width of Order component

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -17,7 +17,7 @@ export default function Order({ pack }: Props) {
   return <>
     <div className="flex flex-col items-center gap-5 lg:min-h-[40rem] lg:bg-white lg:mb-10 lg:flex-row lg:gap-0 lg:items-stretch lg:rounded-xl lg:shadow-md">
 
-      <section className="max-w-2xl w-screen text-lg text-slate-600 lg:min-w-fit lg:w-1/3 lg:py-6">
+      <section className="max-w-2xl w-full text-lg text-slate-600 lg:min-w-fit lg:w-1/3 lg:py-6">
 
         <div className="h-full w-full lg:px-12 lg:py-4 lg:border-r lg:border-slate-200">
 
@@ -50,7 +50,7 @@ export default function Order({ pack }: Props) {
 
       </section>
 
-      <section className="max-w-2xl w-screen mb-10 border-gray-100 lg:pt-10 lg:px-12">
+      <section className="max-w-2xl w-full mb-10 border-gray-100 lg:pt-10 lg:px-12">
 
         <TermsStep
           isVisible={current === 'terms'}
