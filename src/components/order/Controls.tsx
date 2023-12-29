ba9829ed6ab44180fd9455acd7bcd943415caef4
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-29 23:25:48 +0100

    Remove back button from terms step

--- src/components/order/Controls.tsx
+++ src/components/order/Controls.tsx
@@ -1,7 +1,7 @@
 import Button from '../forms/Button'
 
 type Props = {
-  handleBack: () => void,
+  handleBack?: () => void,
   textBack?: string,
   textNext?: string,
 }
@@ -10,7 +10,10 @@ export function Controls({ handleBack, textBack = "Zurück", textNext = "Weiter"
   return <>
     <p className="w-full flex flex-col gap-1 lg:flex-row-reverse lg:justify-end lg:gap-4">
       <Button type="submit">{textNext}</Button>
-      <Button type="button" handleClick={handleBack}>{textBack}</Button>
+      {
+        handleBack &&
+        <Button type="button" handleClick={handleBack}>{textBack}</Button>
+      }
     </p>
   </>
 }
