898abe9f8b02600937e53c153ee6974d922307ba
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-30 13:48:32 +0100

    Create Overview component

---
+++ src/components/order/Overview.tsx
@@ -0,0 +1,39 @@
+import type { CollectionEntry } from 'astro:content'
+
+type Props = {
+  isVisible: boolean,
+  pack: CollectionEntry<'packs'>
+}
+
+export function Overview({ isVisible, pack }: Props) {
+  return <>
+    <div className={isVisible ? "block" : "hidden"}>
+
+      <h1 className="pb-6 font-bold text-2xl text-slate-600">
+        Übersicht
+      </h1>
+
+      <p className="pt-1 flex justify-between gap-8">
+        <span>{pack.data.title}</span>
+        <span>{pack.data.price},00&thinsp;€</span>
+      </p>
+
+      <p className="pb-1 flex justify-between gap-8">
+        <span>Versand (Deutschland)</span>
+        <span>3,00&thinsp;€</span>
+      </p>
+
+      <div className="my-1 w-full h-px bg-slate-300"></div>
+
+      <p className="py-1 flex justify-between gap-8">
+        <span>Gesamt</span>
+        <span>{pack.data.price + 3},00&thinsp;€</span>
+      </p>
+
+      <p className="text-right py-1 text-sm text-slate-400">
+        ggf. zzgl. Auslandsversandkosten
+      </p>
+
+    </div>
+  </>
+}
