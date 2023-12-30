59f226ab12064677e0aa9c17f23b5e1a94c7d219
c-auri <43008483+c-auri@users.noreply.github.com>
2023-12-31 00:40:47 +0100

    Place submit button on the left

--- src/components/order/Controls.tsx
+++ src/components/order/Controls.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function Controls({ formId, handleBack, textBack = "Zurück", textNext = "Weiter" }: Props) {
   return <>
-    <p className="w-full flex flex-col gap-1 lg:flex-row-reverse lg:justify-end lg:gap-4">
+    <p className="w-full flex flex-col gap-1 lg:flex-row lg:justify-start lg:gap-4">
       <Button formId={formId} type="submit">{textNext}</Button>
       {
         handleBack &&
