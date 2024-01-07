0ffca49cf06146b704d9d71c2a2098f199e56f99
c-auri <43008483+c-auri@users.noreply.github.com>
2024-01-07 15:30:45 +0100

    Change menu color to sky blue

--- src/components/page/Breadcrumbs.jsx
+++ src/components/page/Breadcrumbs.jsx
@@ -3,8 +3,8 @@ export default function Breadcrumbs({ navigation }) {
         {
             navigation?.segments.map((segment, segmentIndex) => (
                 <div key={`segment-${segmentIndex}`} className="group flex items-center select-none tracking-wider text-md text-slate-500 fill-slate-500">
-                    <div className="relative cursor-pointer rounded-md hover:fill-slate-500 group-hover:text-slate-500 group-hover:bg-slate-100">
-                        <div className="pl-2 pr-4 py-1 rounded-md flex items-center gap-1 hover:bg-slate-200">
+                    <div className="relative cursor-pointer rounded-md hover:fill-slate-500 group-hover:text-slate-500 group-hover:bg-sky-100">
+                        <div className="pl-2 pr-4 py-1 rounded-md flex items-center gap-1 hover:bg-sky-200">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" viewBox="0 -960 960 960">
                                 <path d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z"/>
                             </svg>
@@ -41,7 +41,7 @@ function Dropdown(navigation, segmentIndex, currentEntry) {
             href={Href(navigation, segmentIndex, entry)}
             key={`${entryIndex}`}
         >
-            <li className={`${rounded} py-1 pl-3 pr-6 flex items-center gap-2 bg-slate-100 hover:text-slate-500 hover:bg-slate-200`}>
+            <li className={`${rounded} py-1 pl-3 pr-6 flex items-center gap-2 bg-sky-100 hover:text-slate-500 hover:bg-sky-200`}>
                 <span className="w-3"></span>
                 <span className={entry.id === currentEntry.id ? "text-slate-900" : ""}>{entry.data.title}</span>
             </li>
