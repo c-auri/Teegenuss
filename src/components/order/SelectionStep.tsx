1c5b4e976e5d986bfe544a6c1ce65964d5dee2f9
c-auri <43008483+c-auri@users.noreply.github.com>
2024-02-03 20:01:40 +0100

    Add selection input

--- src/components/order/SelectionStep.tsx
+++ src/components/order/SelectionStep.tsx
@@ -1,17 +1,27 @@
-import { getEntry, type CollectionEntry } from 'astro:content'
-import { Input } from '../forms/Input'
+import { getCollection } from 'astro:content'
 import { Controls } from './Controls'
+import { useState } from 'react'
 
-const defaultSelection: Pack = await getEntry('packs', '23-1-Chinas-Schaetze')
+const allPacks = await getCollection('packs')
 
-export type Pack = CollectionEntry<'packs'>
+export type Selection = { name: string, price: number, amount: number, max: number }
 
-export function initializeSelection(): Pack[] {
-  return [defaultSelection]
+export function initializeSelection(): Selection[] {
+  return allPacks.reduce<Selection[]>((result, current) => {
+    if (current.data.stash > 0) {
+      result.push({
+        name: current.data.title,
+        price: current.data.price,
+        amount: 0,
+        max: current.data.stash
+      })
+    }
+    return result
+  }, [])
 }
 
 type Props = {
-  initialSelection: Pack[],
+  initialSelection: Selection[],
   handleBack: () => void,
   handleNext: () => void,
 }
@@ -19,6 +29,17 @@ type Props = {
 const formId = "packs-form"
 
 export function SelectionStep({initialSelection, handleBack, handleNext}: Props) {
+  const [selection, setSelection] = useState(initialSelection)
+
+  const setAmount = (pack: Selection, amount: number) => {
+    const newPack = pack
+    newPack.amount = amount
+    const newSelection = [...selection]
+    const index = newSelection.findIndex(p => p.name === pack.name)
+    newSelection.splice(index, 1, newPack)
+    setSelection(newSelection)
+  }
+
   return <>
     <form
       id={formId}
@@ -33,13 +54,11 @@ export function SelectionStep({initialSelection, handleBack, handleNext}: Props)
       <div className="flex-1">
         <div className="flex flex-col justify-start gap-3">
 
-          <Input
-            type="text"
-            label="Paket"
-            value={initialSelection.map(pack => pack.data.title).join(', ')}
-            handleChange={() => undefined}
-            isRequired={true}
-          />
+          {
+            initialSelection.map(pack => <>
+              <Input selection={pack} handleChange={(amount) =>  setAmount(pack, amount)}></Input>
+            </>)
+          }
 
         </div>
       </div>
@@ -48,4 +67,33 @@ export function SelectionStep({initialSelection, handleBack, handleNext}: Props)
 
     </form>
   </>
+}
+
+interface InputProps {
+  selection: Selection,
+  handleChange: (amount: number) => void
+}
+
+function Input({selection, handleChange}: InputProps) {
+  return <span className={`w-full px-8 py-3 text-lg text-slate-600 rounded-md border border-slate-300 grid grid-cols-[3fr,2fr,1fr] gap-10`}>
+    <div className="flex-1">
+      <h3 className="text-slate-700">{selection.name}</h3>
+      <span className="text-slate-500">{selection.price},00&thinsp;€</span>
+    </div>
+    <span className="border rounded-md flex justify-center items-center">
+      <span className="mx-3">Anzahl</span>
+      <input
+        className={`w-10 focus:outline-none`}
+        type="number"
+        onKeyDown={(event) => event.preventDefault()}
+        min="0"
+        max={selection.max}
+        value={selection.amount}
+        onChange={(e) => handleChange(parseInt(e.currentTarget.value))}>
+      </input>
+    </span>
+    <span className="text-right flex justify-end items-center">
+      {selection.amount * selection.price},00&thinsp;€
+    </span>
+  </span>
 }
