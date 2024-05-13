336ca6b8ae3a1c544a479e232e5eace5301de0d4
cauri <43008483+c-auri@users.noreply.github.com>
2024-05-13 16:33:06 +0200

    Move next button to the right

--- src/components/order/Controls.tsx
+++ src/components/order/Controls.tsx
@@ -10,11 +10,11 @@ type Props = {
 export function Controls({ formId, handleBack, textBack = "Zurück", textNext = "Weiter" }: Props) {
   return <>
     <p className="w-full min-h-[6rem] my-6 flex flex-col gap-1 lg:my-0 lg:min-h-0 lg:flex-row lg:justify-start lg:gap-2">
-      <Button formId={formId} type="submit">{textNext}</Button>
       {
         handleBack &&
         <Button type="button" handleClick={handleBack} style="secondary">{textBack}</Button>
       }
+      <Button formId={formId} type="submit">{textNext}</Button>
     </p>
   </>
 }
